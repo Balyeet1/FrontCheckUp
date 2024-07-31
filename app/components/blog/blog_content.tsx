ee230060b1ff770309ce186ee2b3f25dfacffce6
Balyeet1 <ricardoldias123@gmail.com>
2024-07-31 11:09:04 +0000

    Added loading to the CRUD blog buttons.

--- app/components/blog/blog_content.tsx
+++ app/components/blog/blog_content.tsx
@@ -17,11 +17,9 @@ const BlogContent = ({ imageUrl, token, blog }: { imageUrl: string, token: strin
     const [content, setContent] = useState('');
     const [isFetched, setIsFetched] = useState(false)
     const [media, setMedia] = useState<File | null>(null);
-    const fileInputRef = useRef<HTMLInputElement>(null);
     const [mediaUrl, setMediaUrl] = useState<string | null>(null);
     const [previewMode, setPreviewMode] = useState(false);
-
-    const [file, setFile] = useState<any>();
+    const [isLoading, setLoading] = useState<boolean>(false);
 
     useEffect(() => {
         if (blog) {
@@ -33,8 +31,6 @@ const BlogContent = ({ imageUrl, token, blog }: { imageUrl: string, token: strin
                 response.blob().then(blob => {
                     setMedia(new File([blob], blog.image, { type: blob.type }));
                     setMediaUrl(URL.createObjectURL(blob));
-                    setFile(URL.createObjectURL(blob))
-
                 });
             });
         } else setIsFetched(true)
@@ -96,6 +92,7 @@ const BlogContent = ({ imageUrl, token, blog }: { imageUrl: string, token: strin
         }
 
         if (blog && blog.id) {
+            setLoading(true)
             update_user_blog(token, blog.id, formData).then((data: any) => {
                 const { response, blog } = data;
                 if (response) {
@@ -103,6 +100,7 @@ const BlogContent = ({ imageUrl, token, blog }: { imageUrl: string, token: strin
                 }
             });
         } else {
+            setLoading(true)
             create_user_blog(token, formData).then((data: any) => {
                 const { response, blog } = data;
 
@@ -165,8 +163,39 @@ const BlogContent = ({ imageUrl, token, blog }: { imageUrl: string, token: strin
                     </>
                 }
                 <div className='pt-5'>
-                    <Button className="bg-sky-300 hover:bg-sky-400 mr-3" onClick={togglePreviewMode}>{previewMode ? 'Edit' : 'Preview'}</Button>
-                    {previewMode && <Button className='bg-success-400 houver:bg-success-200' onClick={createOrUpdateBlog}>{blog ? 'Update Blog' : 'Create Blog'}</Button>}
+                    <Button
+                        className="bg-sky-300 hover:bg-sky-400 mr-3" onClick={togglePreviewMode}>{previewMode ? 'Edit' : 'Preview'}</Button>
+                    {previewMode &&
+                        <Button
+                            className='bg-success-400 houver:bg-success-200'
+                            onClick={createOrUpdateBlog}
+                            isLoading={isLoading}
+                            spinner={
+                                <svg
+                                    className="animate-spin h-5 w-5 text-current"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <circle
+                                        className="opacity-25"
+                                        cx="12"
+                                        cy="12"
+                                        r="10"
+                                        stroke="currentColor"
+                                        strokeWidth="4"
+                                    />
+                                    <path
+                                        className="opacity-75"
+                                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                        fill="currentColor"
+                                    />
+                                </svg>
+                            }
+                        >
+                            {blog ? `Updat${isLoading ? "ing" : "e"} Blog` : `Creat${isLoading ? "ing" : "e"} Blog`}
+                        </Button>
+                    }
                 </div>
             </div>
         </div>
